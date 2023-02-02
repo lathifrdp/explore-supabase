fa1a7b66dca1e60f753922913231c1dd5c9d3f65
lathifrdp <lathifrdp@gmail.com>
2023-02-02 18:00:53 +0700

    penyesuaian main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_quickstart/pages/account_page.dart';
+import 'package:supabase_quickstart/pages/chat_page.dart';
 import 'package:supabase_quickstart/pages/login_page.dart';
+import 'package:supabase_quickstart/pages/register_page.dart';
 import 'package:supabase_quickstart/pages/splash_page.dart';
+import 'package:supabase_quickstart/utils/constants.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,25 +25,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Supabase Flutter',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.green,
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.green,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.green,
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
+      routes: {
         '/': (_) => const SplashPage(),
         '/login': (_) => const LoginPage(),
         '/account': (_) => const AccountPage(),
+        '/register': (_) => const RegisterPage(),
+        '/chat': (_) => const ChatPage(),
       },
     );
   }
